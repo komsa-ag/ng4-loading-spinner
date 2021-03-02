d362fea5535739cf3efc5475761bb689228174f6
Christian Kropp <christian.kropp@komsa.de>
2021-03-02 08:48:28 +0100

    Support Angular 11

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { Ng4LoadingSpinnerService } from './ng4LoadingSpinner.service';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Ng4LoadingSpinnerComponent } from './ng4LoadingSpinner.component';
+import { Ng4LoadingSpinnerService } from './ng4LoadingSpinner.service';
 
-export * from './ng4LoadingSpinner.service';
 export * from './ng4LoadingSpinner.component';
+export * from './ng4LoadingSpinner.service';
 
 @NgModule({
   imports: [],
@@ -13,7 +13,7 @@ export * from './ng4LoadingSpinner.component';
   providers: [Ng4LoadingSpinnerService]
 })
 export class Ng4LoadingSpinnerModule {
-  static forRoot(): ModuleWithProviders {
+  static forRoot(): ModuleWithProviders<Ng4LoadingSpinnerModule> {
     return {
       ngModule: Ng4LoadingSpinnerModule,
       providers: [Ng4LoadingSpinnerService]
